1788ddea766c403e9ca70cc512d4e94f31829696
S3AN2 <angello.solano@unmsm.edu.pe>
2024-06-12 17:07:28 -0500

    DateUtils Test

--- src/test/java/Junitproject/Junit/DateUtilsTest.java
+++ src/test/java/Junitproject/Junit/DateUtilsTest.java
@@ -1,2 +1,29 @@
-package Junitproject.Junit;public class DateUtilsTest {
+package Junitproject.Junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+    private DateUtils dateUtils= new DateUtils();
+
+    @Test
+    public  void isLeapYearForMultipleFourTest(){
+        Boolean value= dateUtils.isLeapYear(2424);
+        Assert.assertEquals(true,value);
+    }
+    @Test
+    public  void isnotLeapYearForMultipleFourTest(){
+        Boolean value= dateUtils.isLeapYear(123);
+        Assert.assertEquals(false,value);
+    }
+    @Test
+    public  void isnotLeapYearForMultipleOneHundredForNotFourHundredTest(){
+        Boolean value= dateUtils.isLeapYear(2200);
+        Assert.assertEquals(false,value);
+    }
+    @Test
+    public  void isnLeapYearForMultipleOneHundredForFourHundredTest(){
+        Boolean value= dateUtils.isLeapYear(2400);
+        Assert.assertEquals(true,value);
+    }
 }
