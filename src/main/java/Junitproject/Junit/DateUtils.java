fb688cbb543b36d3cda71faf2bed8117e42ec851
S3AN2 <angello.solano@unmsm.edu.pe>
2024-06-12 17:07:08 -0500

    DateUtils Class

--- src/main/java/Junitproject/Junit/DateUtils.java
+++ src/main/java/Junitproject/Junit/DateUtils.java
@@ -1,2 +1,14 @@
-package Junitproject.Junit;public class DateUtils {
+package Junitproject.Junit;
+
+public class DateUtils {
+    public Boolean isLeapYear(int year){
+            if(year%4==0){
+                int date=year/100;
+                if(year%100==0 && date%4!=0){
+                    return  false;
+                }
+                return true;
+            }
+        return false;
+    }
 }
